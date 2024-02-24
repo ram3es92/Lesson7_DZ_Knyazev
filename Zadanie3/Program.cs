6f6e3bf5f2d3d5aef811c5a87ea439346fc0cb19
Roman_Knyazev <ram3es92@mail.ru>
2024-02-24 16:38:58 +0300

    Добавил решение к третьей задаче

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+// Пример: [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+
+void OutputElementsArrayFromEnd(int[] array, int lengthArray)
+{
+    if (lengthArray == 0)
+    {
+        return;
+    }
+    lengthArray = lengthArray - 1;
+    Console.Write($"{array[lengthArray]} ");
+    OutputElementsArrayFromEnd(array, lengthArray);
+}
+
+int[] array = new int[] {1, 2, 5, 0, 10, 34};
+int lengthArray = array.Length;
+OutputElementsArrayFromEnd(array, lengthArray);
