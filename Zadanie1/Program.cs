a4844b3f28175275c6151962e38107470b15f15b
Roman_Knyazev <ram3es92@mail.ru>
2024-02-24 14:33:49 +0300

    Добавил решение по первой задаче

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+// Пример 1)  M = 1; N = 5; -> "1, 2, 3, 4, 5";
+// Пример 2)  M = 4; N = 8; -> "4, 5, 6, 7, 8".
+
+void outputNumbersInBetween(int m, int n)
+{
+    if (m == n)
+    {
+        Console.WriteLine(n);
+        return;
+    }
+    Console.Write(m + ", ");
+    outputNumbersInBetween(m + 1, n);
+}
+
+outputNumbersInBetween(1, 5);
+outputNumbersInBetween(4, 8);
